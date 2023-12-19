f637a67b2e69f144e13af92aab9aed4dc09b17f9
juanpabloestrada <pablo.estrada.1983.18@gmail.com>
2023-12-19 04:00:31 -0600

    Se agregaron los archivos

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import "./Banner.css"
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="banner-container">
+                <h1>Bienvenido a la comunidad</h1>
+                <p>El mejor lugar para encontrar tus libros</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
